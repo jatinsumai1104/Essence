919d48b859df5bc9e4c8d77e5d923ed29ff3e0e9
jatinsumai1104 <jatinsumai1104@gmail.com>
2020-02-05 14:47:30 +0530

    updated config

--- helper/constants.php
+++ helper/constants.php
@@ -1,7 +1,7 @@
 
 <?php 
 
-define("BASEURL", "http://localhost/essence/");
+define("BASEURL", "http://192.168.36.40/essence/");
 define("BASEASSETS", BASEURL."assets/");
 define("BASEPAGES", BASEURL."views/sections/");
 
