ea324f7e6b17dee64f6e8c23046987af9b5e6dec
akshaynavani <akshaynavani1225@gmail.com>
2020-02-05 14:34:06 +0530

    tp

---
+++ classes/controllers/web/Cart.php
@@ -0,0 +1,14 @@
+<?php
+
+Class Cart{
+    protected $di;
+    protected $table = "cart";
+    public function __construct($di){
+        $this->di = $di;
+    }
+
+    public function addCart($data){
+        
+    }
+}
+?>
