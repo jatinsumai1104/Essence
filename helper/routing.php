0f39b3f7725fdc9e3d1900e990569682b51926c4
n7aakash <t2lassi12345@gmail.com>
2020-02-06 04:11:44 +0530

    chatbot implementation phase 1

--- helper/routing.php
+++ helper/routing.php
@@ -67,7 +67,16 @@ if(isset($_POST["place_order"])){
     $di->get('Cart')->deleteCartProductsByUser($_SESSION["user_id"]);
 }
 if(isset($_POST['search_query'])){
-    echo $_POST['search'];
+    // echo $_POST['search'];
     $_SESSION["search_query"]=$_POST['search'];
     Util::redirect("shop");
+}
+if(isset($_POST['chatbot'])){
+    // echo $_POST['chatText'];
+    $word = explode(" ",$_POST['chatText']);
+    // print_r($word);
+    $txt = $word[sizeof($word)-1];
+    $products = $di->get("Database")->rawQuery("SELECT id,product_name,category_name,product_description from product where category_name like '%{$txt}%'");
+    // print_r($products);
+    echo json_encode($products);
 }
