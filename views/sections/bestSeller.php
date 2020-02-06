fc4c7805c41a3c709b1720e40aed8f5920afc57f
jatinsumai1104 <jatinsumai1104@gmail.com>
2020-02-06 10:02:17 +0530

    UI Fixed

--- views/sections/bestSeller.php
+++ views/sections/bestSeller.php
@@ -1,6 +1,6 @@
 <?php
 require_once(__DIR__.'/../includes/header-bp.php');
-$res = $di->get("Product")->bestSeller();
+$products = $di->get("Product")->bestSeller();
 
 ?>
     <!-- ##### Breadcumb Area Start ##### -->
@@ -16,38 +16,73 @@ $res = $di->get("Product")->bestSeller();
         </div>
     </div>
     <!-- ##### Breadcumb Area End ##### -->
-            <?php
-                foreach($res as $r){
-            ?>
-                <div class="col-12 col-sm-6 col-lg-4">
+    <section class="shop_grid_area section-padding-80">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-12 col-lg-12">
+                    <div class="shop_grid_product_area">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="product-topbar d-flex align-items-center justify-content-between">
+                                    <!-- Total Products -->
+                                    <div class="total-products">
+                                        <p><span><?php echo count($products);?></span> products found</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            <?php 
+                                foreach($products as $product){
+                            ?>
+                                <!-- Single Product -->
+                                <div class="col-12 col-sm-6 col-lg-4">
                                     <div class="single-product-wrapper">
                                         <!-- Product Image -->
                                         <div class="product-img">
-                                            <img src="<?php echo 'data:image/jpeg;base64,'.base64_encode( $r['image'] );?>" alt="">
+                                            <img src="<?php echo 'data:image/jpeg;base64,'.base64_encode( $product['image'] );?>" alt="">
                                         </div>
 
                                         <!-- Product Description -->
                                         <div class="product-description">
-                                            <span><?php echo $r['seller_name'];?></span>
-                                            <a href="single-product-details/<?php echo $r['id'];?>">
-                                                <h6><?php echo $r['product_name'];?></h6>
+                                            <span><?php echo $product['seller_name'];?></span>
+                                            <a href="<?php echo BASEURL;?>single-product-details/<?php echo $product['id'];?>">
+                                                <h6><?php echo $product['product_name'];?></h6>
                                             </a>
-                                            <p class="product-price"> Rs. <?php echo $r['price'];?></p>
+                                            <p class="product-price"> Rs. <?php echo $product['price'];?></p>
 
                                             <!-- Hover Content -->
                                             <div class="hover-content">
                                                 <!-- Add to Cart -->
                                                 <div class="add-to-cart-btn">
-                                                <button type="button" name="add_to_cart" class="btn essence-btn add_to_cart" id="<?php echo $r['id']?>" href="#" data-toggle="modal" data-target="#add-cart-modal" class_name="Category">Add to Cart</button>
+                                                    <button type="button" name="add_to_cart" class="btn essence-btn add_to_cart" id="<?php echo $product["id"]?>" href="#" data-toggle="modal" data-target="#add-cart-modal" class_name="Category">Add to Cart</button>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-            <?php
-            
-            }
-            ?>
+                            <?php } ?>
+                        </div>
+                    </div>
+                    <?php if(count($products) > 20){?>
+                    <!-- Pagination -->
+                    <!-- <nav aria-label="navigation">
+                        <ul class="pagination mt-50 mb-70">
+                            <li class="page-item"><a class="page-link" href="#"><i class="fa fa-angle-left"></i></a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">...</a></li>
+                            <li class="page-item"><a class="page-link" href="#">21</a></li>
+                            <li class="page-item"><a class="page-link" href="#"><i class="fa fa-angle-right"></i></a></li>
+                        </ul>
+                    </nav> -->
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </section>
     <!-- ##### Shop Grid Area Start ##### -->
                         </div>
                     </div>
--- views/sections/contact.php
+++ views/sections/contact.php
@@ -5,7 +5,7 @@ require_once(__DIR__.'/../includes/header-bp.php')
     <div class="contact-area d-flex align-items-center">
 
         <div class="google-map">
-            <div id="googleMap"></div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.983462956152!2d72.83367151472946!3d19.06446468709445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c91130392c07%3A0x3c47bf391c8de931!2sThadomal%20Shahani%20Engineering%20College!5e0!3m2!1sen!2sin!4v1580963426625!5m2!1sen!2sin" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
 
         <div class="contact-info">
